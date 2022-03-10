7bb2dd96b087af27ddde27d4d9836c0908d2c3ed
Ricky <31544746+RickyHuang-tech@users.noreply.github.com>
2022-03-10 14:26:26 +0800

    create user api resource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'username' => $this->username,
+            'email' => $this->email,
+            'create_dates' => [
+                'created_at_human' => $this->created_at->diffForHumans()
+            ],
+            'tagline' => $this->tagline,
+            'about' => $this->about,
+            'location' => $this->location,
+            'available_to_hire' => $this->available_to_hire,
+            'formatted_address' => $this->formatted_address
+        ];
+    }
+}
