5e35ee33b5a8388e75641a514e614cb041478f9f
Ricky <31544746+RickyHuang-tech@users.noreply.github.com>
2022-03-10 14:19:32 +0800

    famous "ME" endpoint

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,8 @@ use Illuminate\Support\Facades\Route;
 //        Route::get('/user-profile', [AuthController::class, 'userProfile']);
 //    });
 //});
+Route::get('me','App\Http\Controllers\User\MeController@getMe');
+
 
 Route::group(['middleware'=>['auth:api']],function (){
     Route::post('logout','App\Http\Controllers\Auth\LoginController@logout');
