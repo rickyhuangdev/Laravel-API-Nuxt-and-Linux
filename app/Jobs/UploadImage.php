ea6106db57b522b60ca708c0459f1ff301024111
Ricky <31544746+RickyHuang-tech@users.noreply.github.com>
2022-03-10 16:54:14 +0800

    Resizing uploaded images

--- app/Jobs/UploadImage.php
+++ app/Jobs/UploadImage.php
@@ -2,25 +2,32 @@
 
 namespace App\Jobs;
 
+use App\Models\Design;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-
+use Intervention\Image\Facades\Image;
 class UploadImage implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * @var Design
+     */
+    protected $design;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Design $design)
     {
         //
+        $this->design = $design;
     }
 
     /**
@@ -31,5 +38,20 @@ class UploadImage implements ShouldQueue
     public function handle()
     {
         //
+        $disk = $this->design->disk;
+        $original_file = storage_path().'/uploads/original/'.$this->design->image;
+        try{
+            Image::make($original_file)->fit(800,600,function ($constraint){
+                $constraint->aspectRation();
+            })->save($large = storage_path('uploads/large/'.$this->design->image));
+            //thumbnail
+            Image::make($original_file)->fit(250,200,function ($constraint){
+                $constraint->aspectRation();
+            })->save($large = storage_path('uploads/thumbnail/'.$this->design->image));
+
+        }catch (\Exception $exception){
+            \Illuminate\Log\Logger::error($exception->getMessage());
+
+        }
     }
 }
