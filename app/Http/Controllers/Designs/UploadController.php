650f40c20ab52212495b2b2f40812af804a4b8e2
Ricky <31544746+RickyHuang-tech@users.noreply.github.com>
2022-03-10 16:41:50 +0800

    Uploading image to Temporary Disk

--- app/Http/Controllers/Designs/UploadController.php
+++ app/Http/Controllers/Designs/UploadController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Designs;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\UploadImage;
 use Illuminate\Http\Request;
 
 class UploadController extends Controller
@@ -10,8 +11,21 @@ class UploadController extends Controller
     //
     public function upload(Request $request)
     {
-        $this->validate($request,[
-            'image'=>['required','mime:jpeg,gif,bmp,png','max:2048']
+        $this->validate($request, [
+            'image' => ['required', 'mimes:jpeg,gif,bmp,png', 'max:2048']
         ]);
+        //get the images
+        $image = $request->file('image');
+        $filename = $image->hashName();
+        // move the image to the temporary
+        $tmp = $image->storeAs('uploads/original', $filename, 'tmp');
+        // create the database record for the design
+        $design = auth()->user()->designs()->create([
+            'image' => $filename,
+            'disk' => config('site.upload_disk')
+        ]);
+        //dispatch a job to handle the image manipulation
+        $this->dispatch(new UploadImage($design));
+        return response()->json($design, 200);
     }
 }
